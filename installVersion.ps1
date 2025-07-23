7607f4c1f5d92be500c96f2639b6c74413c02767
vinitamishra15 <64808947+vinitamishra15@users.noreply.github.com>
2025-07-23 14:26:12 +0530

    Create installVersion.ps1

---
+++ installVersion.ps1
@@ -0,0 +1,6 @@
+# installVersion.ps1
+param (
+    [string]$version  # Define a parameter to accept the version
+)
+
+Write-Host "Installing version: $version"  # Write the version passed as argument
