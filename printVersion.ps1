47d1a605ae77ca69e3eebdc1e4e6470ece2a42db
vinitamishra15 <64808947+vinitamishra15@users.noreply.github.com>
2025-07-24 18:32:04 +0530

    Update printVersion.ps1

--- printVersion.ps1
+++ printVersion.ps1
@@ -3,4 +3,4 @@ $newVersion = "1.0.0"
 Write-Host "Generated version: $version"
 
 # Set pipeline output variable
-Write-Host "##vso[task.setvariable variable=version;isOutput=true]$newVersion"
+Write-Host "##vso[task.setvariable variable=version;isOutput=true]1.2.3"
